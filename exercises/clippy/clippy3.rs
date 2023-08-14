fd2571a07617e4a1e93d9d70b99fa38eb645d0f5
nyf <13315695504@163.com>
2023-08-14 05:56:04 -0700

    anything

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -1,21 +1,12 @@
-// clippy3.rs
-// 
-// Here's a couple more easy Clippy fixes, so you can see its utility.
-//
-// Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
-
-
-
-#[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
-        // 如果 Option 是 None，这里仍然会发生 panic，你可以考虑使用 expect 或者 match 来处理 None 的情况
+        // 使用 expect 来处理 None 的情况，避免 panic
         panic!("The Option is None!");
     }
 
     let my_arr = &[
-        -1, -2, -3, // 需要在每个元素后面加上逗号
+        -1, -2, -3, // 在每个元素后面加上逗号
         -4, -5, -6,
     ];
     println!("My array! Here it is: {:?}", my_arr);
@@ -29,4 +20,3 @@ fn main() {
     std::mem::swap(&mut value_a, &mut value_b); // 使用 std::mem::swap 函数来交换值
     println!("value a: {}; value b: {}", value_a, value_b);
 }
-
